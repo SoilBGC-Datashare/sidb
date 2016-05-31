4409d45dd47302378a987a49771fb6fd4a499711
Carlos A. Sierra <csierra@bgc-jena.mpg.de>
2016-05-31 21:54:09 +0200

    Forgot to save

--- scripts/readYaml.R
+++ scripts/readYaml.R
@@ -1,7 +1,8 @@
 
 library(yaml)
 
-tmp=yaml.load_file("~/Repos/sidb/data/Arevalo2012/metadata.yaml")
+arev=yaml.load_file("~/Repos/sidb/data/Arevalo2012/metadata.yaml")
+and=yaml.load_file("~/Repos/sidb/data/Andrews2000SBB/metadata.yaml")
 tmp=yaml.load_file("~/Repos/sidb/data/template_metadata.yaml")
 
 str(tmp)
@@ -9,3 +10,8 @@ str(tmp)
 tmp$siteInfo$coordinates$latitude
 tmp$incubationInfo$carbon
 tmp$incubationInfo$Treatments$Temperature
+
+str(and)
+
+and$variables
+and$incubationInfo$Treatments$Temperature

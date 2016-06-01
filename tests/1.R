c4787274c6fc4c09d9dca3278a25b02431dfa11d
Carlos A. Sierra <csierra@bgc-jena.mpg.de>
2016-06-01 20:43:57 +0200

    Created a test that throws an error if a metatadata file can't be read by R

---
+++ tests/1.R
@@ -0,0 +1,10 @@
+
+# Test that metadata files can be read by R
+
+entryNames=list.dirs("~/sidb/data/", full.names=FALSE, recursive=FALSE)
+
+for(i in 1:length(entryNames)){
+  yaml.load_file(input=paste("~/sidb/data/",entryNames[i],"/metadata.yaml",sep=""))
+  print(paste(entryNames[i],"is ok"))
+  #is.list(tmp)
+}

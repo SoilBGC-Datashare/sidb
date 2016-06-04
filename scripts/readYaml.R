0e369add79259b1d21ffaecaed639f7168da70ec
Carlos A. Sierra <csierra@bgc-jena.mpg.de>
2016-06-04 18:22:53 +0200

    Fixed metadata and created a summary report

--- scripts/readYaml.R
+++ scripts/readYaml.R
@@ -1,17 +1,39 @@
 
+#This script reads all yaml entries in the database, asigns them a name (their own folder name), and puts the output in the global environment
+
 library(yaml)
+entryNames=list.dirs("~/sidb/data/", full.names=FALSE, recursive=FALSE)
+  
+for(i in 1:length(entryNames)){
+    assign(entryNames[i], yaml.load_file(input=paste("~/sidb/data/",entryNames[i],"/metadata.yaml",sep="")))
+  }
 
-arev=yaml.load_file("~/sidb/data/Arevalo2012/metadata.yaml") # Fails
-and=yaml.load_file("~/sidb/data/Andrews2000SBB/metadata.yaml")
-tmp=yaml.load_file("~/sidb/data/template_metadata.yaml")
+citationKey=NULL
+for(i in 1:length(entryNames)){
+  citationKey[i]=get(entryNames[i])$citationKey
+}
 
-str(tmp)
+doi=NULL
+for(i in 1:length(entryNames)){
+  doi[i]=get(entryNames[i])$doi
+}
 
-tmp$siteInfo$coordinates$latitude
-tmp$incubationInfo$carbon
-tmp$incubationInfo$Treatments$Temperature
+entryAuthor=NULL
+for(i in 1:length(entryNames)){
+  entryAuthor[i]=get(entryNames[i])$entryAuthor
+}
 
-str(and)
+entryCreationDate=NULL
+for(i in 1:length(entryNames)){
+  entryCreationDate[i]=get(entryNames[i])$entryCreationDate
+}
 
-and$variables
-and$incubationInfo$Treatments$Temperature
+latitude=NULL
+for(i in 1:length(entryNames)){
+  latitude=c(latitude,get(entryNames[i])$siteInfo$coordinates$latitude)
+}
+
+longitude=NULL
+for(i in 1:length(entryNames)){
+  longitude=c(longitude,get(entryNames[i])$siteInfo$coordinates$longitude)
+}
--- tests/1.R
+++ tests/1.R
@@ -7,6 +7,6 @@ entryNames=list.dirs("~/sidb/data/", full.names=FALSE, recursive=FALSE)
 
 for(i in 1:length(entryNames)){
   yaml.load_file(input=paste("~/sidb/data/",entryNames[i],"/metadata.yaml",sep=""))
-  print(paste(entryNames[i],"is ok"))
+  print(paste(entryNames[i],"can be read by R"))
   #is.list(tmp)
 }

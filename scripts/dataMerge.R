3f8cd5c6c4f0f90ac79626e394239ad073b091a1
Carlos A. Sierra <csierra@bgc-jena.mpg.de>
2016-06-08 15:43:23 +0200

    Added pdf summary and started implementation of data merge

--- scripts/dataMerge.R
+++ scripts/dataMerge.R
@@ -1,9 +1,12 @@
 
 #Create vector with names of all available files
-dataNames=list.dirs("~/sidb/data/", full.names=FALSE, recursive=FALSE)
+entryNames=list.dirs("~/sidb/data/", full.names=FALSE, recursive=FALSE)
 
 # extract data from all existing files and marge into one database
 
-all=read.csv(file=paste("~/Repos/sidb/data/",dataNames,"/data.csv",sep=""))
+all=read.csv(file=paste("~/sidb/data/",entryNames,"/data.csv",sep=""))
 
+ytp=yaml.load_file(input=paste("~/sidb/data/",entryNames[1],"/metadata.yaml",sep=""))
+tmp=read.csv(file=paste("~/sidb/data/",entryNames[1],"/data.csv",sep=""))
 
+ytp[["data"]]<-tmp

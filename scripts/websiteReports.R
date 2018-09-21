62ad053f9574cad4cc5ceea88b8e644ddc93323c
Carlos A. Sierra <csierras@bgc-jena.mpg.de>
2018-09-21 16:26:44 +0200

    Created website for the project

---
+++ scripts/websiteReports.R
@@ -0,0 +1,32 @@
+# This scripts creates summary statistics from the database and outputs it in yaml format
+
+library(yaml)
+
+source("~/sidb/scripts/readYaml.R")
+source("~/sidb/scripts/loadEntries.R")
+
+# Calculate some statistics
+dataPoints=NULL
+nCols=NULL
+for(i in 1:length(entryNames)){
+  mtx=get(entryNames[i])$data
+  nCols[i]=dim(mtx)[2]-1
+  dataPoints[i]=length(as.matrix(mtx))
+  rm(mtx)
+}
+
+smr=list(entries=length(entryNames),timeseries=as.integer(sum(nCols)), datapoints=sum(dataPoints))
+as.yaml(smr)
+
+write_yaml(smr, file="~/sidb/website/_data/summary.yml")
+
+library(maps)
+
+png("~/sidb/website/assets/map.png", bg=NA)
+map('world', interior = FALSE)
+points(longitude, latitude, pch=20, col=2)
+dev.off()
+
+png("~/sidb/website/assets/incubationTime.png", bg=NA)
+hist(incubationTime, xlab="Incubation time (days)", main ="", ylab="Number of studies")
+dev.off()
---
+++ website/_posts/2014-10-29-morbi-a-semper.md
@@ -0,0 +1,8 @@
+---
+layout: post
+title:  "Load all entries in R"
+date:   2018-08-21 14:36:23
+---
+
+The code below gives an example on how to load all the entries of the database and query them in R.
+

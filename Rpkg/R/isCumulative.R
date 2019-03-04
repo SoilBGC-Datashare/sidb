82849752ce18ab6c68749248d243bacb4c6298e6
Carlos A. Sierra <csierra@bgc-jena.mpg.de>
2019-03-04 14:50:08 +0100

    More typos

--- Rpkg/R/isCumulative.R
+++ Rpkg/R/isCumulative.R
@@ -1,11 +1,11 @@
-#' Test whether a time series of CO2 release is cummulative
+#' Test whether a time series of CO2 release is cumulative
 #'
 #' @param x A data.frame with only two columns, first column for time and second column for CO2 release. Test is performed on the second column.
 #' @return logical
 #' @export
 #' @examples
 #' series=data.frame(time=seq(1,10), CO2=cumsum(rep(0.1, 10)))
-#' isCummulative(x=series)
+#' isCumulative(x=series)
 isCumulative=function(x){
   if(class(x) != "data.frame") stop("This function can only be applied to objects of class data.frame")
   if(ncol(x) != 2) stop("This function can only be applied to data.frames with two columns")

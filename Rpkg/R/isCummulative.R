e388b8e23c59f83b0c0d5849a7a38f423fb41108
Carlos A. Sierra <csierra@bgc-jena.mpg.de>
2019-03-04 08:05:41 +0100

    New function to test for cummulative time series

---
+++ Rpkg/R/isCummulative.R
@@ -0,0 +1,13 @@
+#' Test whether a time series of CO2 release is cummulative
+#'
+#' @param x A data.frame with only two columns, first column for time and second column for CO2 release. Test is performed on the second column.
+#' @return logical
+#' @export
+#' @examples
+#' series=data.frame(time=seq(1,10), CO2=cumsum(rep(0.1, 10)))
+#' isCummulative(x=series)
+isCummulative=function(x){
+  if(class(x) != "data.frame") stop("This function can only be applied to objects of class data.frame")
+  if(ncol(x) != 2) stop("This function can only be applied to data.frames with two columns")
+  head(x[,2], 1) < tail(x[,2], 1)
+}

fdb05e7c2f79d04b79f1e542afda45636d2a5710
Carlos A. Sierra <csierras@bgc-jena.mpg.de>
2019-06-25 17:07:55 +0200

    Fixed match between variable names in timeSeries and metadata. Added a test for this

--- Rpkg/tests/testthat/test_loadEntries.R
+++ Rpkg/tests/testthat/test_loadEntries.R
@@ -12,15 +12,34 @@ test_that("entry names correspond to metadata template", {
    entry=database[[i]]
    expect_equal(names(entry)[-(11:12)],names(template))
   }
-  
+
   for(i in 1:length(database)){
     entry=database[[i]]
     expect_equal(names(entry$siteInfo),names(template$siteInfo))
   }
-  
+
   for(i in 1:length(database)){
     entry=database[[i]]
     expect_equal(names(entry$incubationInfo),names(template$incubationInfo))
   }
 })
 
+
+test_that("check first column in timeSeries file is called 'time' ",{
+  database=loadEntries()
+  for(i in 1:length(database)){
+    entry=database[[i]]
+    expect_equal(colnames(entry$timeSeries)[1], "time")
+
+    }
+})
+
+test_that("variable names in timeSeries correspond to names in metadata file",{
+  database=loadEntries()
+  for(i in 1:length(database)){
+    entry=database[[i]]
+    tsName=as.character(colnames(entry$timeSeries))
+    varname=as.character(sapply(entry$variables, function(x){x$name}))
+    expect_equal(tsName, varname)
+  }
+})

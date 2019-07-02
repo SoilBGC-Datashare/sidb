bceac848ec073e4076beb26b3896cde649f1b64c
Carlos A. Sierra <csierras@bgc-jena.mpg.de>
2019-07-02 10:23:14 +0200

    removed symlinks and copied back original yaml files to avoid error in github pages build

--- scripts/databaseSimLink.sh
+++ scripts/databaseSimLink.sh
@@ -1,10 +1,10 @@
 cd ../data/
 names=(*)
-cd ../docs/_data/entries/
+#cd ../docs/_data/entries/
 for file in ${names[*]}; do
 #  wc $file/metadata.yaml
- ln -s ../../../data/$file/metadata.yaml $file.yaml
-# cp  $file/metadata.yaml ../docs/_data/entries/$file.yaml
+# ln -s ../../../data/$file/metadata.yaml $file.yaml
+ cp  $file/metadata.yaml ../docs/_data/entries/$file.yaml
 done
 
 #rm template*

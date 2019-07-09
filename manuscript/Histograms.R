139ffa2d57eb0397d276adf48b876c2f4bb1a1da
Shane Stoner <sstoner@bgc-jena.mpg.de>
2019-07-09 18:34:28 +0200

    Update Histograms.R
    To include all relevant data

--- manuscript/Histograms.R
+++ manuscript/Histograms.R
@@ -1,40 +1,58 @@
-#R codes to reinstruct the histograms in the manuscript
+#R codes to reconstruct the histograms in the manuscript
 devtools::install_github('SoilBGC-Datashare/sidb/Rpkg', force=TRUE)
 library(sidb)
 
-#the database 
-database = loadEntries("~/sidb/data/") 
+#The database
+database = loadEntries("~/sidb/data/")
 
-#Hist1: The incubation time
+#Hist1: Incubation time
 incubTimes=incubationTime(database=database)
 
-# The temperature list
+#Hist2: Incubation temperature
 tempList <- list()
 for(x in database){
   if(is.null(x$incubationInfo$temperature) == FALSE){
-    temper <- x$incubationInfo$temperature
-    tempList <- append(tempList, temper)
-  } else {
-    temper <- x$incubationInfo$laboratoryTreatment$temperature$value
-    tempList <- append(tempList, temper)
+    topLvlTemp <- x$incubationInfo$temperature
+    tempList <- append(tempList, topLvlTemp)
+} else {
+  y <- x$variables
+  varTempList <-list()
+  for(z in y){
+    if(z$name != 'time'){
+      varTempList <- append(varTempList, z$temperature)
+    }
   }
+  varTempList <- unique(varTempList)
+  if(length(varTempList) != 0){
+    tempList <- append(tempList, varTempList)
+  }}
 }
 tempList <- unlist(tempList)
 
-# Soil depth as a site information
-depth= function(database){
-  mean=lapply(database, FUN=function(x){x$siteInfo$depthInfo$midDepth})
-  return(data.frame(depth=unlist(mean)
-  ))
+#Hist 3: Soil depth
+depthList <- list()
+for(x in database){
+  if(is.null(x$incubationInfo$depthInfo$midDepth) == FALSE){
+    depthList <- append(depthList, x$incubationInfo$depthInfo$midDepth)
+  } else {
+  y <- x$variables
+  varDepthList <- list()
+  for(z in y){
+    if(z$name != 'time'){
+      if(is.null(z$midDepth) == FALSE){
+        varDepthList <- append(varDepthList, z$midDepth)
+      }}}
+  varDepthList <- unique(varDepthList)
+  if(length(varDepthList) != 0){
+    depthList <- append(depthList, varDepthList)
+    }}
 }
-depth=depth(database)
+depthList <- unlist(depthList)
 
 #Initial Carbon content, all the units has been converted to the percent
 listC=list()
 for(x in database){
-  if(is.null(levels(x$initConditions$carbonUnits[1]))){
-  }
-  else{
+  if(is.null(levels(x$initConditions$carbonUnits[1])) == FALSE){
     if(x$initConditions$carbonUnits == 'g/gSoil'){
       perC <- as.list(x$initConditions$carbonMean * 100)
       listC <- append(listC,  perC)}
@@ -55,29 +73,28 @@ for(x in database){
       listC <- append(listC, perC)}
   }
 }
-
 listC=unlist(listC)
 
 # Now the Histograms
 
 par(mfrow=c(2,2))
-hist(incubTimes, 
-     freq=FALSE, 
-     main="Incubation Time", 
-     xlab="Time (days)", 
-     border="black", 
+hist(incubTimes,
+     freq=FALSE,
+     main="Incubation Time",
+     xlab="Time (days)",
+     border="black",
      col="gray",
      cex.main=1.4,cex.lab=1.1, cex.axis=0.9,font.lab=2 ,
-     #xlim=c(0,1000),
+     xlim=c(0,1000),
      #ylim = c(0,10),
-     las=1, 
+     las=1,
      breaks=seq(0,1000, l=9))
 
 hist(tempList,
-     freq=FALSE, 
-     main="Incubation Temperature", 
-     xlab="Temperature (˚C)", 
-     border="black", 
+     freq=FALSE,
+     main="Incubation Temperature",
+     xlab="Temperature (˚C)",
+     border="black",
      col="gray",
      cex.main=1.4,cex.lab=1.1, cex.axis=0.9,font.lab=2 ,
      #xlim=c(0,40),
@@ -86,28 +103,28 @@ hist(tempList,
      breaks = seq(0, max(tempList), l=9))
 #breaks=seq(0,max(temperature), l=11))
 
-hist(listC, 
-     freq=FALSE, 
-     main="Initial Soil Carbon Content", 
-     xlab="Percent Carbon", 
-     border="black", 
+hist(listC,
+     freq=FALSE,
+     main="Initial Soil Carbon Content",
+     xlab="Percent Carbon",
+     border="black",
      col="gray",
      cex.main=1.4,cex.lab=1.1, cex.axis=0.9,font.lab=2 ,
      #xlim=c(0,100),
      ylim = c(0,0.1),
-     las=1, 
+     las=1,
      breaks=seq(0,50, l=9))
 
-hist(depth$depth,
-     freq=FALSE, 
-     main="Soil Depth", 
-     xlab="Soil Depth (cm)", 
-     border="black", 
+hist(depthList,
+     freq=FALSE,
+     main="Soil Depth",
+     xlab="Soil Depth (cm)",
+     border="black",
      col="gray",
      cex.main=1.4,cex.lab=1.1, cex.axis=0.9,font.lab=2 ,
      #xlim=c(0,135),
      ylim = c(0,0.04),
-     las=1, 
+     las=1,
      breaks = 9)
 #breaks=seq(0,max(depth$depth), l=11))
 

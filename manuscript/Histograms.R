c9083a7478fa32382637e70ca24816e0d415cdc7
Shane Stoner <sstoner@bgc-jena.mpg.de>
2019-08-28 18:26:06 -0700

    Updating figures and figure code
    New histogram figure (pdf format) is in manuscript folder.

--- manuscript/Histograms.R
+++ manuscript/Histograms.R
@@ -1,9 +1,8 @@
-#R codes to reconstruct the histograms in the manuscript
-devtools::install_github('SoilBGC-Datashare/sidb/Rpkg', force=TRUE)
+#devtools::install_github('SoilBGC-Datashare/sidb/Rpkg', force=TRUE)
 library(sidb)
 
 #The database
-database = loadEntries("~/sidb/data/")
+database = loadEntries("/Users/shane/Desktop/IMPRS/Database/sidb/data/")
 
 #Hist1: Incubation time
 incubTimes=incubationTime(database=database)
@@ -14,20 +13,25 @@ for(x in database){
   if(is.null(x$incubationInfo$temperature) == FALSE){
     topLvlTemp <- x$incubationInfo$temperature
     tempList <- append(tempList, topLvlTemp)
-} else {
-  y <- x$variables
-  varTempList <-list()
-  for(z in y){
-    if(z$name != 'time'){
-      varTempList <- append(varTempList, z$temperature)
+  } else {
+    y <- x$variables
+    varTempList <-list()
+    for(z in y){
+      if(z$name != 'time'){
+        varTempList <- append(varTempList, z$temperature)
+      }
     }
-  }
-  varTempList <- unique(varTempList)
-  if(length(varTempList) != 0){
-    tempList <- append(tempList, varTempList)
-  }}
+    varTempList <- unique(varTempList)
+    if(length(varTempList) != 0){
+      tempList <- append(tempList, varTempList)
+    }}
 }
 tempList <- unlist(tempList)
+
+cexmain=1.9
+cexlab=1.5
+cexaxis=1.19
+fontlab=1.6
 
 #Hist 3: Soil depth
 depthList <- list()
@@ -35,16 +39,16 @@ for(x in database){
   if(is.null(x$incubationInfo$depthInfo$midDepth) == FALSE){
     depthList <- append(depthList, x$incubationInfo$depthInfo$midDepth)
   } else {
-  y <- x$variables
-  varDepthList <- list()
-  for(z in y){
-    if(z$name != 'time'){
-      if(is.null(z$midDepth) == FALSE){
-        varDepthList <- append(varDepthList, z$midDepth)
-      }}}
-  varDepthList <- unique(varDepthList)
-  if(length(varDepthList) != 0){
-    depthList <- append(depthList, varDepthList)
+    y <- x$variables
+    varDepthList <- list()
+    for(z in y){
+      if(z$name != 'time'){
+        if(is.null(z$midDepth) == FALSE){
+          varDepthList <- append(varDepthList, z$midDepth)
+        }}}
+    varDepthList <- unique(varDepthList)
+    if(length(varDepthList) != 0){
+      depthList <- append(depthList, varDepthList)
     }}
 }
 depthList <- unlist(depthList)
@@ -78,38 +82,42 @@ listC=unlist(listC)
 # Now the Histograms
 
 par(mfrow=c(2,2))
-hist(incubTimes,
-     freq=FALSE,
-     main="Incubation Time",
-     xlab="Time (days)",
-     border="black",
-     col="gray",
-     cex.main=1.4,cex.lab=1.1, cex.axis=0.9,font.lab=2 ,
-     xlim=c(0,1000),
-     #ylim = c(0,10),
-     las=1,
-     breaks=seq(0,1000, l=9))
-
 hist(tempList,
      freq=FALSE,
-     main="Incubation Temperature",
-     xlab="Temperature (˚C)",
+     main="Incubation temperature",
+     #xlab= expression(Temperature (~degree~C)),
+     xlab = "Temperature (°C)",
      border="black",
      col="gray",
-     cex.main=1.4,cex.lab=1.1, cex.axis=0.9,font.lab=2 ,
+     cex.main=cexmain,cex.lab=cexlab, cex.axis=cexaxis,font.lab=fontlab,
+     ylab = "Relative density",
      #xlim=c(0,40),
      ylim = c(0,0.05),
      las=1,
      breaks = seq(0, max(tempList), l=9))
+
+hist(incubTimes,
+     freq=FALSE,
+     main="Incubation time",
+     xlab="Time (days)",
+     border="black",
+     col="gray",
+     cex.main=cexmain,cex.lab=cexlab, cex.axis=cexaxis,font.lab=fontlab,
+     ylab = "",
+     xlim=c(0,1000),
+     #ylim = c(0,10),
+     las=1,
+     breaks=seq(0,1000, l=9))
 #breaks=seq(0,max(temperature), l=11))
 
 hist(listC,
      freq=FALSE,
-     main="Initial Soil Carbon Content",
-     xlab="Percent Carbon",
+     main="Initial soil carbon content",
+     xlab="Carbon (%)",
      border="black",
      col="gray",
-     cex.main=1.4,cex.lab=1.1, cex.axis=0.9,font.lab=2 ,
+     cex.main=cexmain,cex.lab=cexlab, cex.axis=cexaxis,font.lab=fontlab,
+     ylab = "Relative density",
      #xlim=c(0,100),
      ylim = c(0,0.1),
      las=1,
@@ -117,11 +125,12 @@ hist(listC,
 
 hist(depthList,
      freq=FALSE,
-     main="Soil Depth",
-     xlab="Soil Depth (cm)",
+     main="Soil depth",
+     xlab="Depth (cm)",
      border="black",
      col="gray",
-     cex.main=1.4,cex.lab=1.1, cex.axis=0.9,font.lab=2 ,
+     cex.main=cexmain,cex.lab=cexlab, cex.axis=cexaxis,font.lab=fontlab,
+     ylab = "",
      #xlim=c(0,135),
      ylim = c(0,0.04),
      las=1,

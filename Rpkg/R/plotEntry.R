52fe864de3d6ef4227d77f9207b07feaeab84ec0
JeffBM <jbeemmil@gmail.com>
2020-02-07 13:26:45 +0100

    added warning message to plotEntry function if multiple units in use in timeseries data

--- Rpkg/R/plotEntry.R
+++ Rpkg/R/plotEntry.R
@@ -10,6 +10,9 @@ plotEntry=function(entry){
   n=ncol(entry$timeSeries)
   ys=entry$timeSeries[,2:n]
 
+  units <- unique(unlist(lapply(entry$variables[2:length(entry$variables)], function(x) unlist(x["units"]))))
+  if(length(units) > 1) message("Warning: Multiple units are present in timeseries data and will be plotted together\n")
+
   matplot(x, ys,type="b",lty=1, pch=19, main=entry$citationKey,
           xlab=paste("Time (",entry$variables$V1$units, ")"),
           ylab=paste(entry$variables$V2$units))

b49f8563557dc2449960d7cbcfd491fa1007cab2
wilarXPS15 <wilarhen@gmail.com>
2021-03-15 21:46:04 -0400

    devel/multiSIdbFit_example.r

--- devel/multiSIdbFit_example.r
+++ devel/multiSIdbFit_example.r
@@ -1,32 +1,32 @@
 ## ---
-## title: "Fitting of multiple time series using 'multiSidbFit'"
+## title: "Example to fit the function 'multiSidbFit'"
 ## author: " "
 ## date: "03/15/2021"
-## output: word_document
 ## ---
+
+## imports
 packs <- c('SoilR', 'sidb', 'yaml', 'parallel','R.utils')
 sapply(packs, require, character.only = TRUE) ## be sure all the outputs are TRUE
-source.R <- '/home/wilar/Documents/sidb/scripts/multiSidbFit'
+
+## Function uploading 
+userLocation <- '/home/wilar/Documents' #Change location of the sidb folder
+source.R <- file.path(userLocation, 'sidb/scripts/multiSidbFit')
 sourceDirectory(source.R, modifiedOnly = TRUE, verbose = TRUE)
 
-## Data entry example 
+## Data-entry loading 
 path <- "/home/wilar/Documents/sidb/data/" ## set a correct file path
 load_entries <- loadEntries(path)
 db <- load_entries[["Crow2019a"]]
 
-
+## Example 1
 twopsFit_model <- multiSidbFit(db,
                        model = 'twopsFit',
                        ic.col = 1,
                        ts.col = 15:20,
                        inipars=c(0.05, 0.00001, 0.1, 0.01))
 
-
+## Example 2
 threeppFit_model <- multiSidbFit(db,
                     model = 'threeppFit',
                     ts.col = 2:10,
                     inipars=c(0.05, 0.01, 0.001, 0.1, 0.1))
-
-
-
-

cfaef11ecb00a9db9673eed5b8d120691c00474a
JeffBM <jbeemmil@gmail.com>
2021-05-12 12:33:39 +0200

    updated comments in data_test.R for clarity

--- tests/data_test.R
+++ tests/data_test.R
@@ -1,6 +1,6 @@
 library(testthat)
 library(sidb)
 
-path_to_data="~/sidb/data/" #Use absolute path where the data is stored
-test_dir("testthat")
+path_to_data <- "~/sidb/data/" # use absolute path for the directory where SIDb data are stored
+test_dir("testthat") # this should be the path to the directory containing the test file "test_dataStructure.R"
 

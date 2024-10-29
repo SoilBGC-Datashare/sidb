c78a4460a8b3ea851822100db9ff2b420d9724e1
Agustín Sarquis <agusarquis@agro.uba.ar>
2024-10-29 12:19:24 -1000

    added Richardson2020

--- tests/data_test.R
+++ tests/data_test.R
@@ -1,6 +1,6 @@
 library(testthat)
 library(sidb)
 
-path_to_data <- "~/sidb/data/" # use absolute path for the directory where SIDb data are stored
-test_dir("testthat") # this should be the path to the directory containing the test file "test_dataStructure.R"
+path_to_data <- "~/GitHub/sidb/data/" # use absolute path for the directory where SIDb data are stored
+test_dir("~/GitHub/sidb/tests/testthat/") # this should be the path to the directory containing the test file "test_dataStructure.R"
 
